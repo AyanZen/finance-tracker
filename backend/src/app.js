e6170cd4a890578a8bab3839110b1bfbeace9138
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2026-04-05 23:30:32 +0530

    server.js and app.js Setup

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(cors({credentials:true}));
+
+app.get("/", (req, res) => {
+    res.send("✅ Backend is running");
+});
+
+export default app;
