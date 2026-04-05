f0fbffba11beecea8966e3f57ed45a737b046af3
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2026-04-05 23:52:10 +0530

    user model with bcrypt

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,2 +1,41 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    }
+},{timestamps:true});
+
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")) return;
+
+    try{
+        const salt = await bcrypt.genSalt(11);
+        this.password = await bcrypt.hash(this.password, salt);
+    }catch(error){
+        console.log(error);
+        throw new Error("Error in hashing password");
+    }
+});
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+
+
+export default mongoose.model('User',userSchema);
